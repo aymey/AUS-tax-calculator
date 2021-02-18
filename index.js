d3e8fdcd6fc200cd6b3374000449f9e6a08c2ad7
Fabian <58243358+FabianPaci@users.noreply.github.com>
2021-02-18 21:06:50 +1100

    2020 - 2021 tax table done
    2020 to 2021 tax calculator is finished

--- index.js
+++ index.js
@@ -1,24 +1,103 @@
-let gross = 12122;
-let deduction = 0;
-let result = 0;
-let taxDed = gross - deduction;
-const taxtable = function () {
-  if (taxDed > 0 && taxDed < 18200) {
-    result = 0;
-    return result;
-  } else if (taxDed > 18200 && taxDed < 37000) {
-    result = texDed + 18200 * 0.19;
-    return result;
-  } else if (taxDed > 37001 && taxDed < 90000) {
-    result = texded + 37000 * 0.325;
-    return result;
-  } else if (taxDed > 90001 && taxDed < 180000) {
-    result = taxDed + 90000 * 0.37;
-    return result;
-  } else if (taxDed > 180001) {
-    result = taxDed + 180000 * 0.45;
-    return result;
-  }
-};
+module.exports = function () {
+  const cli = require("cliutils");
+  const vorpal = require("vorpal")();
+  vorpal.delimiter().show();
 
-return;
+  // const goto = function (url) {
+  //   vorpal
+  //     .command("goto", "goes to a url with your default browser")
+  //     .action(function (args, callback) {
+  //       this.log("goto");
+  //       callback();
+  //     });
+  // };
+
+  // const repl = function () {
+  //   vorpal
+  //     .command("repl", "do maths -original")
+  //     .mode("repl")
+  //     .delimiter("repl:")
+  //     .action(function (command, callback) {
+  //       this.log(eval(command));
+  //     });
+  // };
+
+  // const sql = function () {
+  //   vorpal
+  //     .command("sql", "directly enter arbitrary SQL. -original")
+  //     .mode("sql")
+  //     .delimiter("sql:")
+  //     .init(function (args, callback) {
+  //       this.log(
+  //         "Welcome to SQL mode.\nYou can now directly enter arbitrary SQL commands. To exit, type `exit`."
+  //       );
+  //       callback();
+  //     })
+  //     .action(function (command, callback) {
+  //       var self = this;
+  //       app.query(command, function (res) {
+  //         self.log(res);
+  //         callback();
+  //       });
+  //     });
+  // };
+};
+const vorpal = require("vorpal")();
+vorpal.delimiter().show();
+
+// vorpal
+//   .command("goto", "goes to a url with your default browser")
+//   .action(function (args, callback) {
+//     this.log("goto");
+//     callback();
+//   });
+
+vorpal
+
+  .mode("repl")
+  .description("do maths -original")
+  .delimiter("repl:")
+  .action(function (command, callback) {
+    this.log(eval(command));
+  });
+
+vorpal
+
+  .mode("sql")
+  .description("directly enter arbitrary SQL. -original")
+  .delimiter("sql:")
+  .init(function (args, callback) {
+    this.log(
+      "Welcome to SQL mode.\nYou can now directly enter arbitrary SQL commands. To exit, type `exit`."
+    );
+    callback();
+  })
+  .action(function (command, callback) {
+    var self = this;
+    app.query(command, function (res) {
+      self.log(res);
+      callback();
+    });
+  });
+
+vorpal
+
+  .mode("bash")
+  .description("directly enter arbitrary bash commands. -original")
+  .delimiter("bash:")
+  .init(function (args, callback) {
+    this.log(
+      "Welcome to bash mode.\nYou can now directly enter arbitrary bash commands. To exit, type `exit`."
+    );
+    callback();
+  })
+  .action(function (command, callback) {
+    const k = cp.spawn("bash");
+    k.stdout.pipe(process.stdout);
+    k.stderr.pipe(process.stderr);
+
+    k.stdin.write(command);
+    k.stdin.end("\n");
+
+    k.once("exit");
+  });

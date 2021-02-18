3a7675a551212d6e053d522defb5be931ed16f06
Fabian <58243358+FabianPaci@users.noreply.github.com>
2021-02-18 21:07:34 +1100

    re 2020 - 2021 tax table
     2020 - 2021 tax table again but I put the wrong index so oops

--- index.js
+++ index.js
@@ -1,103 +1,67 @@
-module.exports = function () {
-  const cli = require("cliutils");
-  const vorpal = require("vorpal")();
-  vorpal.delimiter().show();
+let result = 0;
 
-  // const goto = function (url) {
-  //   vorpal
-  //     .command("goto", "goes to a url with your default browser")
-  //     .action(function (args, callback) {
-  //       this.log("goto");
-  //       callback();
-  //     });
-  // };
+const taxtable21 = function (gross, deduction) {
+  const taxDed = gross - deduction;
 
-  // const repl = function () {
-  //   vorpal
-  //     .command("repl", "do maths -original")
-  //     .mode("repl")
-  //     .delimiter("repl:")
-  //     .action(function (command, callback) {
-  //       this.log(eval(command));
-  //     });
-  // };
+  if (taxDed > 0 && taxDed < 18201) {
+    console.log("Bracket: $0 – $18,200");
+    console.log("$0");
+  } else if (taxDed > 18201 && taxDed < 45000) {
+    console.log("Bracket: $18,201 – $45,000");
+    let result = 0.19 * (taxDed - 18200);
+    console.log("$0.19 x (" + taxDed + " - $18,200) =");
+    console.log(result);
 
-  // const sql = function () {
-  //   vorpal
-  //     .command("sql", "directly enter arbitrary SQL. -original")
-  //     .mode("sql")
-  //     .delimiter("sql:")
-  //     .init(function (args, callback) {
-  //       this.log(
-  //         "Welcome to SQL mode.\nYou can now directly enter arbitrary SQL commands. To exit, type `exit`."
-  //       );
-  //       callback();
-  //     })
-  //     .action(function (command, callback) {
-  //       var self = this;
-  //       app.query(command, function (res) {
-  //         self.log(res);
-  //         callback();
-  //       });
-  //     });
-  // };
+    return result;
+  } else if (taxDed > 45001 && taxDed < 120000) {
+    console.log("Bracket: $45,001 – $120,000");
+    let result = 5092 + 0.325 * (taxDed - 45000);
+    console.log("$5,092 + $0.325 x (" + taxDed + " - $45,000) =");
+    console.log(result);
+
+    return result;
+  } else if (taxDed > 120001 && taxDed < 180000) {
+    console.log("Bracket: $120,001 – $180,000");
+    let result = 29467 + 0.37 * (taxDed - 120000);
+    console.log("$29,467 + $0.37 x (" + taxDed + " - $120,000) =");
+    console.log(result);
+
+    return result;
+  } else if (taxDed > 180001) {
+    console.log("Bracket: > $180,001");
+    let result = 51667 + 0.45 * (taxDed - 180000);
+    console.log("$51,667 + $0.45 x (" + taxDed + " - $180,000) = ");
+    console.log(result);
+  }
 };
-const vorpal = require("vorpal")();
-vorpal.delimiter().show();
 
-// vorpal
-//   .command("goto", "goes to a url with your default browser")
-//   .action(function (args, callback) {
-//     this.log("goto");
-//     callback();
-//   });
+const taxtable19 = function (gross, deduction) {
+  const taxDed = gross - deduction;
 
-vorpal
+  if (taxDed > 0 && taxDed < 18201) {
+    console.log("Bracket: $0 – $18,200");
+    console.log("$0");
 
-  .mode("repl")
-  .description("do maths -original")
-  .delimiter("repl:")
-  .action(function (command, callback) {
-    this.log(eval(command));
-  });
+    return result;
+  } else if (taxDed > 18201 && taxDed < 37000) {
+    console.log("Bracket: $18,201 – $37,000");
 
-vorpal
+    return result;
+  } else if (taxDed > 37001 && taxDed < 90000) {
+    console.log("Bracket: $37,001 – $90,000");
 
-  .mode("sql")
-  .description("directly enter arbitrary SQL. -original")
-  .delimiter("sql:")
-  .init(function (args, callback) {
-    this.log(
-      "Welcome to SQL mode.\nYou can now directly enter arbitrary SQL commands. To exit, type `exit`."
-    );
-    callback();
-  })
-  .action(function (command, callback) {
-    var self = this;
-    app.query(command, function (res) {
-      self.log(res);
-      callback();
-    });
-  });
+    return result;
+  } else if (taxDed > 90001 && taxDed < 180000) {
+    console.log("Bracket: $90,001 – $180,000");
 
-vorpal
+    return result;
+  } else if (taxDed > 180001) {
+    console.log("Bracket: > $180,001");
+    return result;
+  }
+};
 
-  .mode("bash")
-  .description("directly enter arbitrary bash commands. -original")
-  .delimiter("bash:")
-  .init(function (args, callback) {
-    this.log(
-      "Welcome to bash mode.\nYou can now directly enter arbitrary bash commands. To exit, type `exit`."
-    );
-    callback();
-  })
-  .action(function (command, callback) {
-    const k = cp.spawn("bash");
-    k.stdout.pipe(process.stdout);
-    k.stderr.pipe(process.stderr);
+// taxtable21(0, 0);
+// taxtable19(0, 0);
 
-    k.stdin.write(command);
-    k.stdin.end("\n");
-
-    k.once("exit");
-  });
+return;
